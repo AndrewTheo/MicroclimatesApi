62fbe30c6f9682cc1116ccce8d344f1abaa35c77
Andrew Theodorides <andrew.theodorides@gmail.com>
2020-05-25 21:17:41 -0700

    Update weather.js

--- pages/api/weather.js
+++ pages/api/weather.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 
 export default (req, res) => {
-  let rawdata = fs.readFileSync("weather.json");
+  let rawdata = fs.readFileSync("./weather.json");
   let weatherData = JSON.parse(rawdata);
   res.json(weatherData);
 }
